8accfeacdededc7a82166bab1d96008e9fce2378
Ikhsan-kun <xsanofficial@gmail.com>
2026-06-28 02:00:32 +0700

    fix3

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '/login-admin',
+        '/login-user',
+        '/reset-password-admin',
+        '/reset-password-user',
     ];
 }
